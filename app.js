3b7001af5726cfa8f7863b3ebfd00c33fbd237f2
Gouda <gouda.clark@gmail.com>
2018-09-11 23:04:59 -0400

    initial pizza express commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// app.js
+
+//require express package
+const express = require('express');
+//save an express module as 'app'
+const app     = express();
+// assigning 3000 as our port
+const PORT    =  process.env.PORT || 3000;
+var hbs = require('hbs');
+
+app.set("view engine", "hbs");
+
+//app.js
+
+app.get("/test/:someValue", (req, res, next) => {
+    res.render("index.hbs", {
+      message: req.params.someValue
+    });
+  });
+
+app.get('/topping/:type', function(req, res, next) {
+
+    res.render("toppings.hbs", {
+        message: req.params.type
+    
+    })
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+
+    res.render("order.hbs", {
+        message: req.params.amount
+    })
+    res.render("order.hbs", {
+        message2: req.params.size
+    })
+    
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Pizza Express!')
+})
+
+app.listen(PORT, function(){
+  console.log("==========================")
+  console.log(`LISTENING ON PORT ${PORT}`);
+  console.log("==========================")
+});
